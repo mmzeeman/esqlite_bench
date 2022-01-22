9c28536819639aa44d4850d3e0ac9c4f93f9eb23
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2022-01-22 16:36:14 +0100

    Don't check the rowid

--- src/esqlite_bench.erl
+++ src/esqlite_bench.erl
@@ -9,35 +9,30 @@ main() ->
     [] = esqlite3:q("create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)", Conn),
 
     Values = lists:seq(1, 10),
-    Range = lists:seq(0, 100_000),
+    Range = lists:seq(1, 1_000_000),
 
-    %% Insert 100_000 records
+    %% Insert 1_000_000 records
     {Time, _} = timer:tc(fun() ->
                                  ok = esqlite3:exec("begin;", Conn),
-                                 {ok, Stmt} = esqlite3:prepare("insert into bench values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?) returning rowid;", Conn),
+
+                                 {ok, Stmt} = esqlite3:prepare("insert into bench values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", Conn),
                                  lists:foreach(fun(_) ->
                                                        ok = esqlite3:bind(Stmt, Values),
-                                                       {row, {_}} = esqlite3:step(Stmt)
+                                                       '$done' = esqlite3:step(Stmt)
                                                end,
                                                Range),
-
-                                 %% make sure to step the statement until a $done is returned,
-                                 %% otherwise it is not possible to comit the transaction.
-                                 '$done' = esqlite3:step(Stmt),
 
                                  ok = esqlite3:exec("commit;", Conn)
                          end),
 
-    erlang:display(commit),
-    io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
+    io:fwrite("1_000_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
-
-    %% Select 100_000 
-    {SelectTime, _} = timer:tc(fun() ->
-                                       esqlite3:q("select * from bench;", Conn)
-                               end),
+    %% Select 1_000_000 
+    {SelectTime, Rows} = timer:tc(fun() ->
+                                          esqlite3:q("select * from bench;", Conn)
+                                  end),
     
-    io:fwrite("100_000 select took: ~p milliseconds~n", [SelectTime/1000]),
+    io:fwrite("Select of ~p rows took: ~p milliseconds~n", [length(Rows), SelectTime/1000]),
 
     ok.
 
