f0c6c8645260cdcba382e4a61c9de23b7eb2a33f
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2022-02-14 22:50:49 +0100

    more sensible amount of inserts

--- src/esqlite_bench.erl
+++ src/esqlite_bench.erl
@@ -1,7 +1,25 @@
 
 -module(esqlite_bench).
 
--export([main/0]).
+-export([main/0, ets/0]).
+
+ets() ->
+    Ref = ets:new(bench, [ordered_set, {keypos, 1}]),
+
+    Values = lists:seq(1, 10),
+    Range = lists:seq(1, 100_000),
+
+    {Time, _} = timer:tc(fun() ->
+                                 lists:foreach(fun(I) ->
+                                                       ets:insert(Ref, list_to_tuple([I] ++ Values))
+                                               end,
+                                               Range)
+                         end),
+
+    io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
+
+    ok.
+
 
 main() ->
     {ok, Conn} = esqlite3:open(":memory:"),
@@ -9,7 +27,7 @@ main() ->
     [] = esqlite3:q("create table bench (a int, b int, c int, d int, e int, f int, g int, h int, i int, j int)", Conn),
 
     Values = lists:seq(1, 10),
-    Range = lists:seq(1, 1_000_000),
+    Range = lists:seq(1, 100_000),
 
     %% Insert 1_000_000 records
     {Time, _} = timer:tc(fun() ->
@@ -25,9 +43,9 @@ main() ->
                                  ok = esqlite3:exec("commit;", Conn)
                          end),
 
-    io:fwrite("1_000_000 inserts took: ~p milliseconds~n", [Time/1000]),
+    io:fwrite("100_000 inserts took: ~p milliseconds~n", [Time/1000]),
 
-    %% Select 1_000_000 
+    %% Select 100_000 
     {SelectTime, Rows} = timer:tc(fun() ->
                                           esqlite3:q("select * from bench;", Conn)
                                   end),
